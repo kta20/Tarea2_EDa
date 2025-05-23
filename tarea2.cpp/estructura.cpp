dd609fed47852bb4e25fda5e52227b0bb9e8b431
kta20 <cfernandezto@usm.cl>
2025-05-22 20:45:33 -0400

    añadiendo estaciones en main()

--- tarea2.cpp/estructura.cpp
+++ tarea2.cpp/estructura.cpp
@@ -44,7 +44,60 @@ public:
 };
 
 int main() {
-    cout << "Creando el árbol ternario...\n";
+const int cant = 19;
+estacion** nodos = new estacion*[cant];
+for (int i = 0; i < cant; ++i) {
+    nodos[i] = nullptr;
+} 
+
+    // Crear habitaciones (nodos)
+    nodos[0] = new estacion(0, "Puente Cal y Canto", "descripcion", "INICIO");
+    nodos[1] = new estacion(1, "Linea 2", "descripcion", "COMBATE");
+    nodos[2]  = new estacion(2, "Santa Ana", "..", "EVENTO");
+    nodos[3] = new estacion(3, "Plaza de Armas", "..", "COMBATE");
+    nodos[4] = new estacion(4, "Baquedano", "..", "COMBATE");
+    nodos[5] = new estacion(5, "Irarrazabal", "..", "EVENTO");
+    nodos[6] = new estacion(6, "Ñuñoa", "..", "FIN");
+    nodos[7] = new estacion(7, "Plaza Egaña", "..", "FIN");
+    nodos[8] = new estacion(8, "Ñuble", "..", "FIN");
+    nodos[9] = new estacion(9, "Tobalaba", "..", "FIN");
+    nodos[10] = new estacion(10, "Bellas Artes", "..", "FIN");
+    nodos[11] = new estacion(11, "Cuming", "..", "FIN");
+    nodos[12] = new estacion(12, "Patronato", "..", "FIN");
+    nodos[13] = new estacion(13, "Linea 3", "..", "COMBATE");
+    nodos[14] = new estacion(14, "Irarrazabal","..", "EVENTO");
+    nodos[15] = new estacion(15, "Plaza de Armas","..", "EVENTO");
+    nodos[16] = new estacion(16, "Pajaritos","..","COMBATE");
+    nodos[17] = new estacion(17, "Estacion Central","..","COMBATE");
+    nodos[18] = new estacion(18, "Baquedano","..","EVENTO");
+
+
+
+    arbolTernario arbol;
+    arbol.asignarRaiz(nodos[0]);
+
+    // Conexiones actualizadas
+    arbol.insertarNodo(nodos[0], nodos[1]);
+    arbol.insertarNodo(nodos[0], nodos[13]);
+    arbol.insertarNodo(nodos[1], nodos[2]);
+    arbol.insertarNodo(nodos[1], nodos[12]);
+    arbol.insertarNodo(nodos[2], nodos[3]);
+    arbol.insertarNodo(nodos[2], nodos[11]);
+    arbol.insertarNodo(nodos[3], nodos[4]);
+    arbol.insertarNodo(nodos[3], nodos[10]);
+    arbol.insertarNodo(nodos[4], nodos[5]);
+    arbol.insertarNodo(nodos[4], nodos[9]);
+    arbol.insertarNodo(nodos[5], nodos[6]);
+    arbol.insertarNodo(nodos[5], nodos[7]);
+    arbol.insertarNodo(nodos[5], nodos[8]);
+    arbol.insertarNodo(nodos[13], nodos[14]);
+    arbol.insertarNodo(nodos[13], nodos[15]);
+    arbol.insertarNodo(nodos[15], nodos[16]);
+    arbol.insertarNodo(nodos[15], nodos[17]);
+    arbol.insertarNodo(nodos[17], nodos[18]);
+    arbol.insertarNodo(nodos[16], nodos[18]);
+
+    cout << "Árbol cargado con 19 estaciones." << endl;
 
     return 0;
 }
